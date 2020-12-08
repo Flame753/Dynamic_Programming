bc8cfffa86d151a96540a5775b17daa6e68132f8
Sergey Gurman <sgurman753@gmail.com>
2020-12-08 13:41:46 -0500

    non-optimize problem

---
+++ Grid_Traveler_Problem/Grid_Traveler.py
@@ -0,0 +1,10 @@
+def grid_traveler(m, n):
+    if m == 1 and n == 1:
+        return 1
+    if m == 0 or n == 0:
+        return 0
+    return grid_traveler(m - 1, n) + grid_traveler(m, n - 1)
+
+
+if __name__ == "__main__":
+    print(grid_traveler(18, 18))
