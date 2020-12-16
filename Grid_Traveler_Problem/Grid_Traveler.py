b477b27b4346ee6e1f2cc7617934e0601f61e2b8
Sergey Gurman <sgurman753@gmail.com>
2020-12-16 16:37:59 -0500

    Grid traveler

--- Grid_Traveler_Problem/Grid_Traveler.py
+++ Grid_Traveler_Problem/Grid_Traveler.py
@@ -1,10 +1,17 @@
-def grid_traveler(m, n):
+def grid_traveler(m, n, memo=None):
+    if memo is None:
+        memo = {}
+
+    key = (m, n)
+    if key in memo:
+        return memo[key]
     if m == 1 and n == 1:
         return 1
     if m == 0 or n == 0:
         return 0
-    return grid_traveler(m - 1, n) + grid_traveler(m, n - 1)
+    memo[key] = grid_traveler(m - 1, n, memo) + grid_traveler(m, n - 1, memo)
+    return memo[key]
 
 
 if __name__ == "__main__":
-    print(grid_traveler(18, 18))
+    print(grid_traveler(300, 320))
